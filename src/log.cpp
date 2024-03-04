fd071a4d62becf12a32b8b717f909b9a52d3e423
devandriev <devandriev@gmail.com>
2024-03-04 11:09:17 +0200

    Add miscelaneous fixes

--- src/log.cpp
+++ src/log.cpp
@@ -1,9 +1,20 @@
 #include "log.hpp"
 
-void logc(char* msg) {
-    std::cout << msg << std::endl;
+namespace dbg {
+    void logc(char* msg) {
+        std::cout << msg << std::endl;
+    }
+
+    void log(std::string str_msg) {
+        std::cout << str_msg << std::endl;
+    }
+
+    void lerr(std::string err_msg) {
+        std::cout << "ERROR: " << err_msg << std::endl;
+    }
+
+    void lerrc(char* err_msg) {
+        std::cout << "ERROR ( via lerrc() ): " << err_msg << std::endl;
+    }
 }
 
-void log(std::string str_msg) {
-    std::cout << str_msg << std::endl;
-}
--- src/log.hpp
+++ src/log.hpp
@@ -3,5 +3,14 @@
 #include <chrono>
 #include <iomanip>
 
-void logc(char* msg);
-void log(std::string str_msg);
+namespace dbg {
+    // Log-level log (C impl)
+    void logc(char* msg);
+    // Log-level log (C++ impl)
+    void log(std::string str_msg);
+    
+    // Error-level log (C++ impl)
+    void lerr(std::string err_msg);
+    // Error-level log (C impl)
+    void lerrc(char* err_msg);
+}
